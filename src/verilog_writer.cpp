f97d3863120d85e10446f3ca4af6689e65ea5587
Kyle Burge <kyle.burge7196@gmail.com>
2018-06-26 09:29:15 -0400

    Fully loads verilog template now.

--- src/verilog_writer.cpp
+++ src/verilog_writer.cpp
@@ -39,7 +39,7 @@ namespace abaqs {
     }
     templ.close();
 
-    if(parts.size() != 8) {
+    if(parts.size() != 7) {
       throw CompilerRuntimeError(
         "Rules template has an invalid number of sections.");
     }
@@ -59,17 +59,32 @@ namespace abaqs {
   }
 
   std::string
-  VerilogWriter::gatherNextTemplateSection(std::ifstream& templ)
+  VerilogWriter::gatherNextTemplateSection(
+    std::ifstream& templ)
   {
-    templ.seekg(0, templ.end);
-    templ.get();
-    return std::string("Testing\n");
+    const std::string seperator {"\n<<<>>>\n\n"};
+
+    std::string result;
+
+    while(!templ.eof()) {
+      result.push_back(templ.get());
+      int index = result.length() - seperator.length();
+      if(index < 0) continue;
+      std::string end = result.substr(index);
+      if(seperator == end) {
+        return result.substr(0, index);
+      }
+    }
+
+    // Last character will be eof. We don't need that.
+    result.erase(result.length() - 1, result.npos);
+    return std::move(result);
   }
 
   std::string
   VerilogWriter::generate()
   {
-    return std::string("");
+    return std::string("Hello world.");
   }
 
   CompilerRuntimeError::CompilerRuntimeError(std::string str)
